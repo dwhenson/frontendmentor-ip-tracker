2895dfc501fb4baf6ef5f80e7eaae4c1f02d63f4
dwhenson <dave.henson@gmail.com>
2021-05-11 21:18:08 +0300

    ☑️ ADD: Loading text for HTML and IP regex pattern

--- js/app.js
+++ js/app.js
@@ -11,16 +11,21 @@ const ip = document.querySelector("[data-ip]");
 const ipLocation = document.querySelector("[data-location]");
 const utc = document.querySelector("[data-utc]");
 const isp = document.querySelector("[data-isp]");
+const regex = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
 
 /* Maps
 /* ==================================================== */
 const mymap = L.map("mapid", { zoomControl: false }).setView([37.419857, -122.078827], 10);
-const popup = L.popup();
+const mapIcon = L.icon({ iconUrl: "./../images/icon-location.svg" });
 
 /* ====================================================
    Functions
    ==================================================== */
 
+/**
+ * Render the fetched data to HTML
+ * @param      {Array}  data    The array of fetched data
+ */
 function renderDataIP(data) {
 	ip.textContent = `${data.ip}`;
 	ipLocation.textContent = `${data.location.city}, ${data.location.region}, ${data.location.country}`;
@@ -28,14 +33,23 @@ function renderDataIP(data) {
 	isp.textContent = `${data.isp}`;
 }
 
+/**
+ * Update the map tiles and marker location based on fetched data
+ * @param      {Array}  data    The array of fetched data
+ */
 function updateMap(data) {
 	mymap.setView([data.location.lat, data.location.lng], 14);
 	L.tileLayer(
 		"https://tile.thunderforest.com/neighbourhood/{z}/{x}/{y}.png?apikey=1078500321124a708f0fa546fff421f0"
 	).addTo(mymap);
-	L.marker([data.location.lat, data.location.lng]).addTo(mymap);
+	L.marker([data.location.lat, data.location.lng], { icon: mapIcon }).addTo(mymap);
 }
 
+/**
+ * Fetches the data on the IP address
+ * @param      {String}   endpoint  The url endpoint
+ * @return     {Array}		data 			The parse array of data
+ */
 async function fetchData(endpoint) {
 	const response = await fetch(endpoint);
 	const data = await response.json();
@@ -45,9 +59,16 @@ async function fetchData(endpoint) {
 
 /* Handlers
 /* ==================================================== */
+
+/**
+ * Handle the input event to update the IP being searched
+ * @param      {Object}  event   The event object
+ */
 function inputHandler(event) {
 	event.preventDefault();
 	if (!event.target.closest("form")) return;
+	// TODO: add check of below against regex, and fetch only if true
+	console.log(event.target.querySelector("input").value);
 	fetchData(`https://geo.ipify.org/api/v1?apiKey=${apiKey}&ipAddress=${ipAddressInput.value}`);
 }
 
@@ -58,7 +79,7 @@ function inputHandler(event) {
 /* IP address
    ---------------- */
 document.addEventListener("submit", inputHandler);
-fetchData(`https://geo.ipify.org/api/v1?apiKey=${apiKey}&ipAddress=8.8.8.8`);
+fetchData(`https://geo.ipify.org/api/v1?apiKey=${apiKey}&ipAddress=`);
 
 /* Maps
    ---------------- */
